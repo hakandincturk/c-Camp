368f849313bec690b90188dbe6d779e797a958ba
Hakan DİNÇTÜRK <sorumsuzoyuncu@hotmail.com>
2021-01-23 14:29:42 +0300

    myDic add

---
+++ Dic/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Dic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDic<int, string> days = new MyDic<int, string>();
+            days.Add(1, "Pazartesi");
+            days.Add(2, "Salı");
+
+            Console.WriteLine(days.Count);
+            days.Add(3, "Çarş");
+
+            for (int i = 0; i < days.Count; i++)
+            {
+                Console.WriteLine("[" + days.Keys[i] + ": " + days.Values[i] + "]");
+            }
+        }
+    }
+}
